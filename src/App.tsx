d0d6afbfcd5c8d5037d2d135c9ae0c5f0d3183ac
BADRINATHDUBE17 <BADRINATHDUBE17@GMAIL.COM>
2025-03-28 17:05:46 +0530

    App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Groups from "./pages/masters/Groups";
+import Ledger from "./pages/masters/Ledger";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          
+          {/* Masters Routes */}
+          <Route path="/masters/groups" element={<Groups />} />
+          <Route path="/masters/ledger" element={<Ledger />} />
+          
+          {/* Add other routes here */}
+          
+          {/* Catch-all route */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
