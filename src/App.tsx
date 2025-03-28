71c8af367eab864a51b0608f851c2ce93c469fc4
BADRINATHDUBE17 <BADRINATHDUBE17@GMAIL.COM>
2025-03-28 17:09:01 +0530

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -9,9 +8,10 @@ import Dashboard from "./pages/Dashboard";
 import Groups from "./pages/masters/Groups";
 import Ledger from "./pages/masters/Ledger";
 import NotFound from "./pages/NotFound";
+import Index from "./pages/Index";
 
+// Create a client
 const queryClient = new QueryClient();
-
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
@@ -36,5 +36,32 @@ const App = () => (
     </TooltipProvider>
   </QueryClientProvider>
 );
+function App() {
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            
+            {/* Masters Routes */}
+            <Route path="/masters/groups" element={<Groups />} />
+            <Route path="/masters/ledger" element={<Ledger />} />
+            
+            {/* Add other routes here */}
+            
+            {/* Catch-all route */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
 
 export default App;
